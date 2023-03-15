829e79162d33645dad92a6f841769083ca1a2c14
Cristian David Araujo <cristian.araujo@udea.edu.co>
2023-03-15 18:05:03 -0500

    Inicio proyecto

---
+++ Practica1-2/Practica1-2.srcs/sources_1/new/dataFlow.vhd
@@ -0,0 +1,28 @@
+
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity dataFlow is
+    Port ( A : in STD_LOGIC;
+           B : in STD_LOGIC;
+           C : in STD_LOGIC;
+           Fout : out STD_LOGIC);
+end dataFlow;
+
+architecture Behavioral of dataFlow is
+
+begin
+
+
+end Behavioral;
