0d55b1d9f89e49bef4e91740af9bfd504611e2a7
Josxiu <100869137+Josxiu@users.noreply.github.com>
2023-04-25 02:29:43 -0500

    Update completeAssembly_tb.vhd
    In the testbench all 8 ALU operations are tested and for each one, the 4 possible cases for the inputs that can reach the ALU for each bus are tested.

--- practica3/practica3.srcs/sources_1/new/sim_1/new/completeAssembly_tb.vhd
+++ practica3/practica3.srcs/sources_1/new/sim_1/new/completeAssembly_tb.vhd
@@ -43,14 +43,14 @@ architecture Behavioral of completeAssembly_tb is
     -- Inputs
     signal add_A : STD_LOGIC_VECTOR(2 downto 0) := (others => '0');
     signal add_B : STD_LOGIC_VECTOR(1 downto 0) := (others => '0');
-    signal FA : STD_LOGIC := '0';
-    signal FB : STD_LOGIC := '0';
-    signal en0 : STD_LOGIC := '1';
-    signal en1 : STD_LOGIC := '1';
-    signal en2 : STD_LOGIC := '1';
+    --signal FA : STD_LOGIC;
+    --signal FB : STD_LOGIC;
+    signal en0 : STD_LOGIC;
+    signal en1 : STD_LOGIC;
+    signal en2 : STD_LOGIC;
     signal sel_ALU : STD_LOGIC_VECTOR (2 downto 0) := (others => '0');
-    signal dataA : STD_LOGIC_VECTOR (3 downto 0) := "1000"; -- Initial value is 8
-    signal dataB : STD_LOGIC_VECTOR (3 downto 0) := "0101"; -- Initial value is 5
+    signal dataA : STD_LOGIC_VECTOR (3 downto 0) := "1011"; -- Initial value is 11
+    signal dataB : STD_LOGIC_VECTOR (3 downto 0) := "0111"; -- Initial value is 7
     
     signal clk : STD_LOGIC := '0'; -- the clock signal starts at 0
     
@@ -60,8 +60,35 @@ architecture Behavioral of completeAssembly_tb is
     signal activeDisplay : STD_LOGIC_VECTOR(3 downto 0);
 
     -- clock period definition
-    constant clk_period : time := 30 ns; -- 33.33 MHz clock frequency
+    --constant clk_freq : integer := 10;
+    constant clk_period : time := 30 ns; -- 33.3 MHz
     -- this constant is used to generate the clock signal
+
+    signal FAFB : STD_LOGIC_VECTOR(1 downto 0) := "00"; -- this signal is used to control the inputs of the ALU
+
+
+    -- Procedure to increment a counter
+    procedure increment_counter(signal counter : inout STD_LOGIC_VECTOR(2 downto 0)) is
+        begin
+            if counter = "111" then
+                counter <= "000";
+            else
+                counter <= counter + 1;
+            end if;
+        end procedure;
+
+    -- Procedure to increment add_A and add_B
+    procedure increment(signal add_A : inout STD_LOGIC_VECTOR(2 downto 0); signal add_B : inout STD_LOGIC_VECTOR(1 downto 0);signal FAFB : in STD_LOGIC_VECTOR(1 downto 0)) is
+        begin
+            if FAFB = "01" then
+                add_A <= add_A + 1;
+                add_B <= add_B + 1;
+            elsif FAFB = "00" then
+                add_A <= add_A + 1;
+            elsif FAFB = "11" then
+                add_B <= add_B + 1;
+            end if;
+    end procedure;
 
 begin
 
@@ -69,8 +96,10 @@ begin
     uut: completeAssembly port map (
         add_A => add_A,
         add_B => add_B,
-        FA => FA,
-        FB => FB,
+        --FA => FA,
+        --FB => FB,
+        FA => FAFB(1),
+        FB => FAFB(0),
         en0 => en0,
         en1 => en1,
         en2 => en2,
@@ -83,44 +112,47 @@ begin
         activeDisplay => activeDisplay
     );
 
+    
+
+
     -- clock generation
     clk <= not clk after clk_period / 2; -- 50% duty cycle clock
     
-    -- testbench stimulus process
-    process is
-        variable FAB : STD_LOGIC_VECTOR(1 downto 0) := (others => '0');
+    -- Stimulus process
+    -- Using loops, we will test all 8 ALU operations, and in each operation we will test the 4 possible cases, romA and dataB, romA and romB, dataA and dataB, and finally dataA and romB.
+    -- FA and FB control the input data, when FA is 0 the input is romA, and when FB is 0 the input is dataB.
+    -- So we are going to test all the possible combinations of FA and FB, and we will test all the possible combinations of sel_ALU.
+    stim_proc: process
+    variable count : integer := 0;
     begin
-        wait for 100 ns;
-        -- First we use a for loop to test all the possible values of the selection of the ALU
+
+        wait until rising_edge(clk);
+        wait until rising_edge(clk);
+
+
+        -- This loop test all the values of sel_ALU
         for i in 0 to 7 loop
-            sel_ALU <= std_logic_vector(to_unsigned(i, 3)); -- This way we can test all the possible values of the ALU selection
-            -- Now in each operation we test the possible 4 cases of the inputs A and B for the ALU 
-            -- romA and dataB: FA = '0', FB = '0'
-            -- romA and romB: FA = '0', FB = '1'
-            -- dataA and dataB: FA = '1', FB = '0'
-            -- dataA and romB: FA = '1', FB = '1'
+            -- Here we change the value of sel_ALU in each iteration
+            sel_ALU <= std_logic_vector(to_unsigned(i, 3));
 
-            -- loop to test all the 4 cases of the inputs A and B for the ALU
-            while FAB < "11" loop
-                FA <= FAB(1);
-                FB <= FAB(0);
-                FAB := FAB + 1; -- We increment the value of the variable FAB to test all the possible values
-                wait for 100 ms; -- We wait 100 ms to see the result in the display
-            end loop;
-            -- We increment this signals to test different values
-            add_A <= add_A + 1;
-            add_B <= add_B + 1; 
-            dataA <= dataA + 1; 
-            dataB <= dataB + 1; 
+            -- We change the value of add_A and add_B in each iteration so we can test different values
+            increment(add_A, add_B, FAFB); -- Here we change the values of add_A and add_B
+
+            en0 <= '1';
+            en1 <= '1';
+            en2 <= '0';
+            
             wait until rising_edge(clk);
-
+            en2 <= '1';
+            wait until rising_edge(clk);
+            
         end loop;
-        wait;
+        
+        FAFB <= FAFB + 1; -- Here we change the values of FA and FB
+        dataA <= dataA + 1; -- Here we change the value of dataA
+        dataB <= dataB + 1; -- Here we change the value of dataB
 
+       
     end process;
-   
-
-
-
 
 end architecture Behavioral;
