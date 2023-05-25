b0bd5c1f33e8a87f3dc751517039743ce78307e6
Cristian David Araujo <cristian.araujo@udea.edu.co>
2023-05-24 22:16:42 -0500

    Agregamos el teclado

--- Basys3_Master4.xdc
+++ Basys3_Master4.xdc
@@ -9,10 +9,10 @@ set_property PACKAGE_PIN W5 [get_ports clk]
 	create_clock -add -name sys_clk_pin -period 10.00 -waveform {0 5} [get_ports clk]
  
 ## Switches
-set_property PACKAGE_PIN V17 [get_ports {sw[0]}]					
-	set_property IOSTANDARD LVCMOS33 [get_ports {sw[0]}]
-set_property PACKAGE_PIN V16 [get_ports {sw[1]}]					
-	set_property IOSTANDARD LVCMOS33 [get_ports {sw[1]}]
+#set_property PACKAGE_PIN V17 [get_ports {sw[0]}]					
+#	set_property IOSTANDARD LVCMOS33 [get_ports {sw[0]}]
+#set_property PACKAGE_PIN V16 [get_ports {sw[1]}]					
+#	set_property IOSTANDARD LVCMOS33 [get_ports {sw[1]}]
 #set_property PACKAGE_PIN W16 [get_ports {COLOR[2]}]					
 #	set_property IOSTANDARD LVCMOS33 [get_ports {COLOR[2]}]
 #set_property PACKAGE_PIN W17 [get_ports {COLOR[3]}]					
@@ -271,12 +271,12 @@ set_property PACKAGE_PIN R19 [get_ports vsync]
 
 
 ##USB HID (PS/2)
-#set_property PACKAGE_PIN C17 [get_ports PS2Clk]						
-	#set_property IOSTANDARD LVCMOS33 [get_ports PS2Clk]
-	#set_property PULLUP true [get_ports PS2Clk]
-#set_property PACKAGE_PIN B17 [get_ports PS2Data]					
-	#set_property IOSTANDARD LVCMOS33 [get_ports PS2Data]	
-	#set_property PULLUP true [get_ports PS2Data]
+set_property PACKAGE_PIN C17 [get_ports PS2Clk]						
+	set_property IOSTANDARD LVCMOS33 [get_ports PS2Clk]
+	set_property PULLUP true [get_ports PS2Clk]
+set_property PACKAGE_PIN B17 [get_ports PS2Data]					
+	set_property IOSTANDARD LVCMOS33 [get_ports PS2Data]	
+	set_property PULLUP true [get_ports PS2Data]
 
 
 ##Quad SPI Flash
--- practica4/practica4.srcs/sources_1/new/main.vhd
+++ practica4/practica4.srcs/sources_1/new/main.vhd
@@ -5,9 +5,10 @@ use IEEE.STD_LOGIC_ARITH.ALL;
 
 entity main is
     Port ( clk, reset: in std_logic;
-		   sw: in std_logic_vector(1 downto 0);
 		   hsync, vsync: out std_logic;
-		   rgb_out: out std_logic_vector(11 downto 0));
+		   rgb_out: out std_logic_vector(11 downto 0);
+           
+           ps2_clk, ps2_data: in std_logic);
 end main;
 
 architecture Behavioral of main is
@@ -44,51 +45,73 @@ architecture Behavioral of main is
     );
     end component;
     
-    signal Hcount, Vcount, posX, posY:  std_logic_vector (10 downto 0);
+    signal Hcount, Vcount, posXBird, posYBird:  std_logic_vector (10 downto 0);
 	signal RGBin : STD_LOGIC_VECTOR (11 downto 0);
-	signal clk20ns, clk60ms, drawBird : STD_LOGIC;
-	
+	signal clk20ns, clk60ms, drawBird, asciiStart : STD_LOGIC;
+	signal asciiData : STD_LOGIC_VECTOR (6 downto 0);
 begin
 
-    Inst_vga_ctrl_640x480_60Hz: vga_ctrl_640x480_60Hz PORT MAP(
-		rst => reset,
-		clk => clk20ns,
-		rgb_in => RGBin,
-		HS => hsync,
-		VS => vsync,
-		hcount => Hcount,
-		vcount => Vcount,
-		rgb_out => rgb_out,
-		blank => open
-	);
+    Inst_vga_ctrl_640x480_60Hz: vga_ctrl_640x480_60Hz 
+        port map(
+            rst => reset,
+            clk => clk20ns,
+            rgb_in => RGBin,
+            HS => hsync,
+            VS => vsync,
+            hcount => Hcount,
+            vcount => Vcount,
+            rgb_out => rgb_out,
+            blank => open
+        );
 	
-	--Instans of clock
+
     --Clock 20ns
-    -- generador de reloj de 50 MHZ
     process (clk)
         begin  
             if (clk'event and clk = '1') then
                 clk20ns <= NOT clk20ns;
             end if;
         end process;
-    
+    --Instans of clock
     --Clock 60ms
     clkDivider1ms : clkDivider
-                generic map (countLimit => 3000000)
-                Port map (clk => clk20ns,
-                         newClk => clk60ms);
+        generic map (countLimit => 3000000)
+        port map(clk => clk20ns,
+                newClk => clk60ms);
                          
     draw_Bird1: draw_Bird
-                port map( clk => clk60ms,
-                          posX => posX,
-                          posY => posY,
-                          Hcount => Hcount,
-                          Vcount => Vcount,
-                          draw => drawBird);
+        port map( clk => clk60ms,
+                  posX => posXBird,
+                  posY => posYBird,
+                  Hcount => Hcount,
+                  Vcount => Vcount,
+                  draw => drawBird);
+
+    keyboard: keyboard2ASCII
+        port map( clk => clk20ns;
+                  ps2_clk => ps2_clk;
+                  ps2_data => ps2_data;
+                  ascii_new => asciiStart;
+                  ascii_code => asciiData);
+
+    keyboardPress: process(asciiStart, asciiData)
+        begin
+            if (asciiStart = '1') then
+                if ((asciiData = x"57" or asciiData = x"77") and (posY + 15 <= 439)) then
+                    posYBird <= posYBird + 1;
+                elsif ((asciiData = x"53" or asciiData = x"73") and (posY >= 0 )) then
+                    posYBird <= posYBird - 1;
+                elsif ((asciiData = x"41" or asciiData = x"61") and (posX >= 0)) then
+                    posXBird <= posXBird - 1;
+                elsif ((asciiData = x"44" or asciiData = x"64") and (posX + 15 <= 679)) then
+                    posXBird <= posXBird + 1;
+            end if;
+        end process;
                           
-    posX <= (others => '0');
-    posY <= (others => '0');                  
+    posXBird <= (others => '0');
+    posYBird <= (others => '0');                  
     
-    RGBin <= x"000" when (drawBird = '1' and posX + 15 >= Hcount and posY + 15 >= Vcount)  else x"FFF";
+    RGBin <= x"000" when (drawBird = '1' and posXBird + 15 >= Hcount and posYBird + 15 >= Vcount)  else
+            x"FFF";
  
 end Behavioral;
