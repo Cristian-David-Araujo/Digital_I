944e7f5a3fb9e90795e079ae1cc8be9a43f2eee9
Cristian David Araujo Araujo <51882823+Cristian-David-Araujo@users.noreply.github.com>
2023-05-30 15:44:09 -0500

    Update drawWood.vhd

--- practica4/practica4.srcs/sources_1/new/displays/drawWood.vhd
+++ practica4/practica4.srcs/sources_1/new/displays/drawWood.vhd
@@ -1,3 +1,6 @@
+--This code is to control the positions in which the character will be displayed,
+--Taking the information from the drawing of a ROMs
+
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.STD_LOGIC_UNSIGNED.ALL;
@@ -14,7 +17,7 @@ entity drawWood is
 end drawWood;
 
 architecture Behavioral of drawWood is
-
+    --Instans of ROM
     component ROM_WOOD is
     port(
         address : in STD_LOGIC_VECTOR (4 downto 0);
@@ -50,41 +53,13 @@ begin
         data => dataROM2
     );
 
-
+    --Control of ROMS positions to be read out
     addressY_aux <= Vcount - posY;
     addressY <= addressY_aux(4 downto 0);
     addressX_aux <= Hcount - posX;
     addressX <= addressX_aux(5 downto 0);
 
-    --data <= dataROM
-    --'1' when (data(to_integer(unsigned(addressX(3 downto 0)))) = '1') and (posX + 15 >= Hcount and posX <= Hcount and posY + 15 >= Vcount and posY <= Vcount) else '0'
-
---    process(dataROM,dataROM2,addressX,addressY,posX,posY,Hcount,Vcount)
---    variable drawrom1,drawrom2 : STD_LOGIC;
---    variable position : boolean;
-
---    begin
---        drawrom1 := dataROM(to_integer(unsigned(addressX(5 downto 0))));
---        drawrom2 := dataROM2(to_integer(unsigned(addressX(5 downto 0))));
---        position := (posX + 39 >= Hcount and posX <= Hcount and posY + 31 >= Vcount and posY <= Vcount);
-
---        -- This enables the draw of the wood
---        if ((drawrom1 = '1' or drawrom2 = '1') and position = true) then
---            drawwood <= '1';
---        else
---            drawwood <= '0';
---        end if;
-
---        -- This decides the color painted
---        if drawrom1 = '1' and drawrom2 = '1' then
---            woodcolor <= X"730";
---        elsif drawrom1 = '0' and drawrom2 = '1' then
---            woodcolor <= X"D96";
---        else
---            woodcolor <= X"fff";
---        end if;
-
---    end process;
+    --The output of the ROM information is conditioned so that the image is not repeated in all possible positions.
 
     drawwood <= '1' when dataROM(to_integer(unsigned(addressX(5 downto 0)))) = '1' and (posX + 39 >= Hcount and posX <= Hcount and posY + 31 >= Vcount and posY <= Vcount) else '0';
     draw <= drawwood;
